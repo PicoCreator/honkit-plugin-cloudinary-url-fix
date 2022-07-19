1a96eb990e3eefeeb2fe9765ea6f708c4b06c83a
Eugene Cheah <PicoCreator@users.noreply.github.com>
2022-07-19 22:58:28 +0800

    Properly setting up as a hook

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 module.exports = {
-	"page": function(page) {
-		
-		// This searches and matches against the following 
-		//
-		// /                                                        # Start of regex
-		// (                                                        # Capture group 1
-		//     https:\/\/[a-zA-Z0-9_\-\.]*\.cloudinary\.com\/       # https://anything.cloudinary.com/
-		//     [a-zA-Z0-9_\-\/\.]*                                  # any/number/of/uri/path, without the character "\"
-		// )
-		// (                                                        # Capture group 2
-		//	   \\\_                                                 # Match against "\_" specifically
-		// )
-		// /g                                                       # End of regex, matches globally
-		//
-		let matchingRegex = /(https:\/\/[a-zA-Z0-9_\-\.]*\.cloudinary\.com\/[a-zA-Z0-9_\-\/\.]*)(\\\_)/g;
+	hooks: {
+		"page:before": function(page) {
+			
+			// This searches and matches against the following 
+			//
+			// /                                                        # Start of regex
+			// (                                                        # Capture group 1
+			//     https:\/\/[a-zA-Z0-9_\-\.]*\.cloudinary\.com\/       # https://anything.cloudinary.com/
+			//     [a-zA-Z0-9_\-\/\.]*                                  # any/number/of/uri/path, without the character "\"
+			// )
+			// (                                                        # Capture group 2
+			//	   \\\_                                                 # Match against "\_" specifically
+			// )
+			// /g                                                       # End of regex, matches globally
+			//
+			let matchingRegex = /(https:\/\/[a-zA-Z0-9_\-\.]*\.cloudinary\.com\/[a-zA-Z0-9_\-\/\.]*)(\\\_)/g
 
-		// This replacement str uses the above regex
-		// Keeps capture group 1 content, while replacing "\_" with "_" respectively, finishing the fix
-		let replacmentStr = "$1_"
-		
-		// Perform the replacement, before processing the page markdown
-		page.content = page.content.replaceAll(matchingRegex, replacmentStr)
-		return page;
+			// This replacement str uses the above regex
+			// Keeps capture group 1 content, while replacing "\_" with "_" respectively, finishing the fix
+			let replacmentStr = "$1_"
+			
+			// Perform the replacement, before processing the page markdown
+			page.content = page.content.replaceAll(matchingRegex, replacmentStr)
+			return page;
+		}
 	}
 }
