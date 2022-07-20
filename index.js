5dd6c4a74078c9cecfb87a02c7bf54e76a0d1410
Eugene Cheah <PicoCreator@users.noreply.github.com>
2022-07-20 10:28:26 +0800

    Expending to all valid URI characters

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ module.exports = {
 			
 			// This searches and matches against the following 
 			//
-			// /                                                        # Start of regex
-			// (                                                        # Capture group 1
-			//     https:\/\/[a-zA-Z0-9_\-\.]*\.cloudinary\.com\/       # https://anything.cloudinary.com/
-			//     [a-zA-Z0-9_\-\/\.]*                                  # any/number/of/uri/path, without the character "\"
+			// /                                                                 # Start of regex
+			// (                                                                 # Capture group 1
+			//     https:\/\/[a-zA-Z0-9_\-\.]*\.cloudinary\.com\/                #   https://anything.cloudinary.com/
+			//     [a-zA-Z0-9\-\.\_\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\%\=]*    #   all/valid/%uri/encoded/characters, without the character "\"
 			// )
-			// (                                                        # Capture group 2
-			//	   \\\_                                                 # Match against "\_" specifically
+			// (                                                                  # Capture group 2
+			//	   \\\_                                                           #   Match against "\_" specifically
 			// )
-			// /g                                                       # End of regex, matches globally
+			// /g                                                                 # End of regex, matches globally
 			//
-			let matchingRegex = /(https:\/\/[a-zA-Z0-9_\-\.]*\.cloudinary\.com\/[a-zA-Z0-9_\-\/\.]*)(\\\_)/g
+			let matchingRegex = /(https:\/\/[a-zA-Z0-9_\-\.]*\.cloudinary\.com\/[a-zA-Z0-9\-\.\_\~\:\/\?\#\[\]\@\!\$\&\'\(\)\*\+\,\;\%\=]*)(\\\_)/g
 
 			// This replacement str uses the above regex
 			// Keeps capture group 1 content, while replacing "\_" with "_" respectively, finishing the fix
@@ -25,7 +25,7 @@ module.exports = {
 			let newContent = page.content.replaceAll(matchingRegex, replacmentStr)
 			while( newContent !== page.content ) {
 				page.content = newContent;
-				newContent = page.content.replaceAll(matchingRegex, replacmentStr);
+				newContent = page.content.replaceAll(matchingRegex, replacmentStr);  
 			}
 
 			return page;
